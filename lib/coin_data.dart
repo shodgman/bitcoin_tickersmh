304b47a271730d01f0f400323ddc7166500abd6b
shodgman <34950809+shodgman@users.noreply.github.com>
2021-10-05 17:46:15 +1100

    Tuesday catch up

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,8 @@
+//TODO: Add your imports here.
+import '../utilities/networking.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +33,61 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const coinHostSite = 'rest.coinapi.io';
+const coinExtended = '/v1/exchangerate';
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = '5F6AED68-1ED3-4F85-8D04-C46B32EBE12F';
+
+class CoinData {
+  //TODO: Create your getCoinData() method here.
+  // getCoinData() method
+  //
+  // requires crypto currency and actual currency codes
+  // Returns a Future dynamic that is JSON coded or a return error number
+  ///////////////////////////////////
+  Future getCoinData() async {
+    // These will be parameters
+    String crypto = 'BTC';
+    String currency = 'USD';
+
+    // Construct the url string
+    String myExtended =
+        'https://$coinHostSite$coinExtended/$crypto/$currency?apikey=$apiKey';
+    print(myExtended);
+    myExtended =
+        'https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=5F6AED68-1ED3-4F85-8D04-C46B32EBE12F';
+    // Query and response
+    // Await the http get response, then decode the json-formatted response.
+    //http.Response response = await http.get(Uri.parse(myExtended));
+    NetworkHelper httpHelper = NetworkHelper(myExtended);
+    var response = await httpHelper.getData();
+    // decode response: extract
+    if (response.statusCode == 200) {
+      print('Response = 200');
+      String data = response.body;
+
+      //print('JSON Version');
+      //print(jsonDecode(data));
+      //print(jsonDecode(data));
+      return data;
+
+      return jsonDecode(data) ?? '{}';
+    } else {
+      // error
+      print('Error response ${response.statusCode}');
+    }
+    // return response
+  }
+}
+//  Documentation of the coinAPI syntax
+// curl https://rest.coinapi.io/v1/exchangerate/BTC/USD \
+// --request GET
+// --header "X-CoinAPI-Key: 73034021-THIS-IS-SAMPLE-KEY"
+// The above command returns JSON structured like this:
+//
+// {
+// "time": "2017-08-09T14:31:18.3150000Z",
+// "asset_id_base": "BTC",
+// "asset_id_quote": "USD",
+// "rate": 3260.3514321215056208129867667
+// }
---
+++ lib/utilities/networking.dart
@@ -0,0 +1,47 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+////////////////////////////////////////////////
+// A Network helper Class
+//
+//  Provides services for http stuff
+//
+//////////////////////////////////////////////
+
+class NetworkHelper {
+  NetworkHelper(this.url);
+
+  final String url;
+
+  Future getData() async {
+    Uri uri = Uri.parse(url);
+    http.Response response = await http.get(uri);
+    if (response.statusCode == 200) {
+      print(response.body);
+      String data = response.body;
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+      return null;
+    }
+  }
+
+  // void Mymain(List<String> arguments) async {
+  //   // This example uses the Google Books API to search for books about http.
+  //   // https://developers.google.com/books/docs/overview
+  //   var url =
+  //       Uri.https('www.googleapis.com', '/books/v1/volumes', {'q': '{http}'});
+  //
+  //   // Await the http get response, then decode the json-formatted response.
+  //   var response = await http.get(url);
+  //   if (response.statusCode == 200) {
+  //     var jsonResponse =
+  //         convert.jsonDecode(response.body) as Map<String, dynamic>;
+  //     var itemCount = jsonResponse['totalItems'];
+  //     print('Number of books about http: $itemCount.');
+  //   } else {
+  //     print('Request failed with status: ${response.statusCode}.');
+  //     throw Exception('API Request Failed');
+  //   }
+  // }
+}
